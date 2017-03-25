a883c1e6a62bfc3fe3697e0040011bf195403625
jonny <jonnyspitzner@googlemail.com>
2017-03-25 18:53:08 +0100

    add npm and grunt stuff

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        options:{
+          style:'compressed'
+        },
+        files: {
+          'css/shadow.min.css' : 'scss/shadow.scss'
+        }
+      }
+    },
+    autoprefixer:{
+      dist:{
+        files:{
+          'css/shadow.min.css':'css/shadow.min.css'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: 'scss/*.scss',
+        tasks: ['sass', 'autoprefixer']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.registerTask('default',['watch']);
+}
