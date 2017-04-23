abdd7fa18a12748614faea9d2da27e6e2b9297f1
jonny <jonnyspitzner@googlemail.com>
2017-04-23 08:59:03 +0200

    add autoprefixer options

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,9 @@ module.exports = function(grunt) {
       }
     },
     autoprefixer:{
+      options: {
+                browsers: ['last 10 versions']
+        },
       dist:{
         files:{
           'css/shadow.min.css':'css/shadow.min.css'
